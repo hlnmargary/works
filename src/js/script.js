f8e43477eb6a66162c6f6a6e3f379d6b39bb6ae2
hlnmargary <margary.helene@hetic.net>
2019-03-18 13:47:27 +0100

    mobile version fixed, without backgrounds

--- src/js/script.js
+++ src/js/script.js
@@ -2,25 +2,3 @@
 
 AOS.init();
 
-var svg = document.getElementById("morph");
-var s = Snap(svg);
-
-var simpleCup = Snap.select('#jacket');
-var fancyCup = Snap.select('#sneakers');
-
-var simpleCupPoints = simpleCup.node.getAttribute('g');
-var fancyCupPoints = fancyCup.node.getAttribute('g');
-
-
-var toFancy = function () {
-  simpleCup.animate({ g: fancyCupPoints }, 1000, mina.backout, toSimple);
-}
-
-var toSimple = function () {
-  simpleCup.animate({ g: simpleCupPoints }, 1000, mina.backout, toFancy);
-}
-
-
-toSimple();
-
-
