497abf59050bf40806cf8a160b60b0232d84ae15
hlnmargary <margary.helene@hetic.net>
2019-03-21 17:34:52 +0100

    darkmode ok

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,12 @@
 AOS.init();
 console.log('link');
 
+var all = document.querySelector('.all');
+var darkButton = document.querySelector('.darkButton');
 
+
+darkButton.addEventListener('click', function () {
+  console.log('ok');
+  all.classList.toggle('darkmode');
+  console.log('toggle');
+});
